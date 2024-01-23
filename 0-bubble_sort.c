0831109e4bf8e1dcbcfe0cdf62110e2444e02a05
meskana <marcelinusilonuba21@gmail.com>
2024-01-23 12:41:17 -0800

    task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include"sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+	
+	if (size < 2 || !(array))
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
